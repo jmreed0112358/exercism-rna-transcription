d4d20f984b99f63e962ef8d98b7d693fef49708b
Jeremy M. Reed <reeje76@gmail.com>
2016-06-30 15:26:17 -0600

    Add the rest of the files.

---
+++ src/main/java/rnatranscription/RnaTranscription.java
@@ -0,0 +1,10 @@
+package rnatranscription;
+
+import exceptions.NotImplementedException;
+
+public class RnaTranscription
+{
+	public static String ofDna(String input) {
+		throw new NotImplementedException();
+	}
+}
--- src/test/java/rnatranscription/RnaTranscriptionTest.java
+++ src/test/java/rnatranscription/RnaTranscriptionTest.java
@@ -1,3 +1,4 @@
+package rnatranscription;
 import org.junit.Assert;
 import org.junit.Test;
 
