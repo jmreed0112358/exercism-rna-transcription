a02918e0bab14ea99cf9e0f3396f254b50807fae
Jeremy M. Reed <reeje76@gmail.com>
2016-07-01 12:15:01 -0600

    Implement RnaTranscription

--- src/main/java/rnatranscription/RnaTranscription.java
+++ src/main/java/rnatranscription/RnaTranscription.java
@@ -1,10 +1,51 @@
 package rnatranscription;
 
+import java.security.InvalidParameterException;
+
 import exceptions.NotImplementedException;
 
 public class RnaTranscription
 {
+	private static Character getTranslation(Character input) {
+		switch ( input ) {
+			case 'G':
+				return 'C';
+			case 'C':
+				return 'G';
+			case 'T':
+				return 'A';
+			case 'A':
+				return 'U';
+			default:
+				throw new InvalidParameterException();
+		}
+	}
+	
 	public static String ofDna(String input) {
-		throw new NotImplementedException();
+		if ( input == null) {
+			throw new NullPointerException();
+		}
+		
+		input = sanitize(input);
+		
+		StringBuilder sb = new StringBuilder();
+		for ( int i = 0 ; i < input.length( ) ; i++ ) {
+			sb.append( getTranslation( input.charAt( i ) ) );
+		}
+		
+		return sb.toString( );
+	}
+	
+	private static String sanitize(String input) {
+		StringBuilder sb = new StringBuilder();
+		
+		input = input.toUpperCase( );
+		for ( int i = 0 ; i < input.length( ) ; i++ ) {
+			if ( input.charAt( i ) == 'G' || input.charAt( i ) == 'C' || input.charAt( i ) == 'T' || input.charAt( i ) == 'A' ) {
+				sb.append( input.charAt( i ) );
+			}
+		}
+		
+		return sb.toString( );
 	}
 }
